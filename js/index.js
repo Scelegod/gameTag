9a727a687c2b19ad35af6afca987cb6a87fc2f09
Scelegod <thescelegod@mail.ru>
2023-03-03 10:49:10 +0300

    start2

--- js/index.js
+++ js/index.js
@@ -1,25 +1,34 @@
 "use strict"
 
-// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
-// let page = document.querySelector('#page');
-// let all__buttons = document.querySelector('.all__buttons');
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+let page = document.querySelector('#page');
+let all__buttons = document.querySelector('.all__buttons');
+//Размер ячейки
+let btnDimensions = 100;
 
-// for(let elem of arr){
-//     let button = document.createElement('button');
-//     button.textContent = elem;
-//     button.setAttribute('class', 'item');
-//     all__buttons.appendChild(button);
-//     button.addEventListener('click', function(){
-//         console.log(button.textContent);
+for(let elem of arr){
+    let button = document.createElement('button');
+    button.textContent = elem;
+    button.className = 'button';
+    all__buttons.append(button);
 
-//     });
-// }
-// all__buttons.lastElementChild.remove();
+    //Распложение ячеек 4 на 4
+    let left = elem % 4;
+    let top = (elem - left) / 4;
+    
+    //Показ ячеек 
+    button.style.left = `${left * btnDimensions}px`;
+    button.style.top = `${top * btnDimensions}px`;
 
-// let values = new Array(16).fill(0).map((item, index) => index + 1);
-// for(let val in values){
-//     let button = document.createElement('button');
-//     button.textContent = val;
-//     button.setAttribute('class', 'item');
-//     all__buttons.appendChild(button);
-// }
+
+    button.addEventListener('click', function(){
+        console.log(button.textContent);
+        
+    }); 
+
+
+}
+
+function movingBtn(){
+
+}
